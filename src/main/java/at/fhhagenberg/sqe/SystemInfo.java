6636d63d8f167339ec00462b76fe1b726bdc3468
Maike <maike.rieger1@gmail.com>
2020-10-16 22:54:42 +0200

    added javadoc

--- src/main/java/at/fhhagenberg/sqe/SystemInfo.java
+++ src/main/java/at/fhhagenberg/sqe/SystemInfo.java
@@ -1,11 +1,22 @@
 package at.fhhagenberg.sqe;
 
+/**
+ * Gather Systeminfo
+ */
 public class SystemInfo {
 
+    /**
+     * Get java version
+     * @return current java version
+     */
     public static String javaVersion() {
         return System.getProperty("java.version");
     }
 
+    /**
+     * Get javaFX version
+     * @return current javaFX version
+     */
     public static String javafxVersion() {
         return System.getProperty("javafx.version");
     }
