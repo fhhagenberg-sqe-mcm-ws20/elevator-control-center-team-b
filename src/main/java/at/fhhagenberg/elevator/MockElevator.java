4ada08a2bfba4539160775159d9ba5eea2d960a0
Andreas Roither <andi.roither@protonmail.com>
2020-10-19 17:30:57 +0200

    Add elevator functions for mock test

--- src/main/java/at/fhhagenberg/elevator/MockElevator.java
+++ src/main/java/at/fhhagenberg/elevator/MockElevator.java
@@ -2,7 +2,7 @@ package at.fhhagenberg.elevator;
 
 /**
  * Elevator example class
- *
+ * <p>
  * This elevator presumes that the initial state is:
  * - on floor 0
  * - no speed
@@ -24,6 +24,12 @@ public class MockElevator implements IElevator {
     private boolean[] floorServices;
     private int floorTarget;
 
+    /**
+     * Default constructor where no specific state is used
+     * @param floors Amount of floors the elevator serves
+     * @param weight Weight of the elevator
+     * @param capacity Capacity of the elevator
+     */
     public MockElevator(int floors, int weight, int capacity) {
         this.direction = Direction_State.uncommitted;
         this.acceleration = 0;
@@ -38,6 +44,35 @@ public class MockElevator implements IElevator {
         this.floorTarget = 0;
     }
 
+    /**
+     * Constructor to set a specific elevator state
+     * @param direction Current direction of the elevator
+     * @param acceleration Acceleration of the elevator
+     * @param buttons Active buttons
+     * @param doorState Door state of the elevator
+     * @param nearestFloor Floor that is closest to the elevator in feet
+     * @param positionFromGround Position of the elevator from ground in feet
+     * @param speed Current speed of the elevator
+     * @param weight Overall weight of the elevator with no passengers
+     * @param capacity Capacity of the elevator (amount of people)
+     * @param floorServices Floors that the elevator stops at
+     * @param floorTarget Current active target the elevator will go to
+     */
+    public MockElevator(Direction_State direction, int acceleration, int[] buttons, Door_State doorState, int nearestFloor, int positionFromGround, int speed, int weight, int capacity, boolean[] floorServices, int floorTarget) {
+        this.direction = direction;
+        this.acceleration = acceleration;
+        this.buttons = buttons;
+        this.doorState = doorState;
+        this.nearestFloor = nearestFloor;
+        this.positionFromGround = positionFromGround;
+        this.speed = speed;
+        this.weight = weight;
+        this.capacity = capacity;
+        this.floorServices = floorServices;
+        this.floorTarget = floorTarget;
+    }
+
+    //region ElevatorAPI
     @Override
     public int getCommittedDirection() {
         return direction.value();
@@ -111,4 +146,48 @@ public class MockElevator implements IElevator {
     public void setTarget(int floor) {
         floorTarget = floor;
     }
+    //endregion
+
+
+    //region Mock
+    public void setDirection(Direction_State direction) {
+        this.direction = direction;
+    }
+
+    public void setAcceleration(int acceleration) {
+        this.acceleration = acceleration;
+    }
+
+    public void setButtons(int[] buttons) {
+        this.buttons = buttons;
+    }
+
+    public void setDoorState(Door_State doorState) {
+        this.doorState = doorState;
+    }
+
+    public void setNearestFloor(int nearestFloor) {
+        this.nearestFloor = nearestFloor;
+    }
+
+    public void setPositionFromGround(int positionFromGround) {
+        this.positionFromGround = positionFromGround;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setFloorServices(boolean[] floorServices) {
+        this.floorServices = floorServices;
+    }
+    //endregion
 }
