e28a387149837e5b5ec07522ab2cd0e046abe2c4
Andreas Roither <andi.roither@protonmail.com>
2020-10-20 09:51:05 +0200

    Fix html for javadoc in elevator api
    Add documentation for elevator

--- src/main/java/at/fhhagenberg/elevator/IElevator.java
+++ src/main/java/at/fhhagenberg/elevator/IElevator.java
@@ -1,25 +1,57 @@
 package at.fhhagenberg.elevator;
 
+/**
+ * Elevator interface
+ */
 public interface IElevator {
 
     /**
      * Enum for state of elevator doors
      */
     enum Door_State {
+        /**
+         * Open door state
+         */
         open(1),
+        /**
+         * Closed door state
+         */
         closed(2),
+        /**
+         * Door is opening state
+         */
         opening(3),
+        /**
+         * Door is closing state
+         */
         closing(4);
 
-        private int state;
-        Door_State(final int value){
+        private final int state;
+
+        /**
+         * Default constructor
+         *
+         * @param value state value
+         */
+        Door_State(final int value) {
             this.state = value;
         }
 
+        /**
+         * Get numerical value of the door state
+         *
+         * @return value
+         */
         public int value() {
             return this.state;
         }
 
+        /**
+         * Create enum from value
+         *
+         * @param value door state value
+         * @return door state or default value
+         */
         public Door_State setValue(int value) {
             switch (value) {
                 case 1:
@@ -40,19 +72,45 @@ public interface IElevator {
      * Enum state for direction of the elevator
      */
     enum Direction_State {
+        /**
+         * Up direction state of the elevator
+         */
         up(0),
+        /**
+         * Down state of the elevator
+         */
         down(1),
+        /**
+         * Uncommitted state of the elevator
+         */
         uncommitted(2);
 
-        private int state;
-        Direction_State(final int value){
+        private final int state;
+
+        /**
+         * Default constructor
+         *
+         * @param value state
+         */
+        Direction_State(final int value) {
             this.state = value;
         }
 
+        /**
+         * Get numerical value of the door state
+         *
+         * @return value
+         */
         public int value() {
             return this.state;
         }
 
+        /**
+         * Create enum from value
+         *
+         * @param value direction state value
+         * @return direction state or default value
+         */
         public Direction_State createFromValue(int value) {
             switch (value) {
                 case 0:
@@ -67,18 +125,104 @@ public interface IElevator {
         }
     }
 
+    /**
+     * Direction of the elevator
+     *
+     * @return direction state
+     */
     int getCommittedDirection();
+
+    /**
+     * Acceleration of the elevator
+     *
+     * @return acceleration
+     */
     int getAccel();
+
+    /**
+     * Status of a button for a certain floor
+     *
+     * @param floor requested floor
+     * @return state
+     */
     boolean getButtonStatus(int floor);
+
+    /**
+     * Door status of the elevator
+     *
+     * @return door status
+     */
     int getDoorStatus();
+
+    /**
+     * Nearest floor to the elevator
+     *
+     * @return nearest floor
+     */
     int getCurrentFloor();
+
+    /**
+     * Position of the elevator from ground in feet
+     *
+     * @return position
+     */
     int getPosition();
+
+    /**
+     * Current speed of the elevator
+     *
+     * @return speed
+     */
     int getSpeed();
+
+    /**
+     * Weight of the elevator
+     *
+     * @return weight
+     */
     int getWeight();
+
+    /**
+     * Allowed capacity of the elevator
+     *
+     * @return capacity
+     */
     int getCapacity();
+
+    /**
+     * Check if elevator serves floor
+     *
+     * @param floor floor number
+     * @return true | false
+     */
     boolean servesFloor(int floor);
+
+    /**
+     * Returns the current target of the elevator
+     *
+     * @return target
+     */
     int getTarget();
+
+    /**
+     * Set the direction of the elevator
+     *
+     * @param direction direction state
+     */
     void setDirection(int direction);
+
+    /**
+     * Set if an elevator serves a particular floor
+     *
+     * @param floor   floor number
+     * @param service service
+     */
     void setServicesFloor(int floor, boolean service);
+
+    /**
+     * Sets target of the elevator
+     *
+     * @param floor floor
+     */
     void setTarget(int floor);
 }
--- src/main/java/at/fhhagenberg/elevatorservice/IElevatorSystem.java
+++ src/main/java/at/fhhagenberg/elevatorservice/IElevatorSystem.java
@@ -4,71 +4,71 @@ package at.fhhagenberg.elevatorservice;
  * High-level interface to access and control the elevator system.
  * 
  * <p>The elevator's behavior is impacted by the following parameters:
- * <ul><i>Elevator capacity.</i> The maximum number of passengers that can fit on an 
- * elevator.</ul>
- * <ul><i>Elevator speed.</i> The maximum speed an elevator can travel at in feet/sec. 
- * It is assumed to be the same for all elevators.</ul>
- * <ul><i>Elevator acceleration.</i> The rate at which the elevator can increase or decrease
+ * <ul><li><i>Elevator capacity.</i> The maximum number of passengers that can fit on an 
+ * elevator.</li></ul>
+ * <ul><li><i>Elevator speed.</i> The maximum speed an elevator can travel at in feet/sec. 
+ * It is assumed to be the same for all elevators.</li></ul>
+ * <ul><li><i>Elevator acceleration.</i> The rate at which the elevator can increase or decrease
  * speed in ft/sec2. It is assumed to be the same for all elevators. The higher the acceleration,
- * the faster the elevator can accelerate and decelerate, allowing its average speed to be faster.</ul>
- * <ul><i>Number of floors.</i> The number of floors in the building, including the ground floor. It is
+ * the faster the elevator can accelerate and decelerate, allowing its average speed to be faster.</li></ul>
+ * <ul><li><i>Number of floors.</i> The number of floors in the building, including the ground floor. It is
  * assumed there are no floors below ground level. All elevators service the
  * ground floor but may, as described below, not necessarily service all other floors. Each floor has 
  * an up and a down button for the purposes of passengers calling the elevator. Floor numbering starts 
- * at 0 for floor 1.</ul> 
- * <ul><i>Floor height.</i> The height of the floors in feet. It is assumed that each floor is 
- * the same height.</ul>
- * <ul><i>Number of elevators.</i> The number of elevators in the building. Elevator numbering starts 
- * at zero for elevator 1.</ul>
+ * at 0 for floor 1.</li></ul> 
+ * <ul><li><i>Floor height.</i> The height of the floors in feet. It is assumed that each floor is 
+ * the same height.</li></ul>
+ * <ul><li><i>Number of elevators.</i> The number of elevators in the building. Elevator numbering starts 
+ * at zero for elevator 1.</li></ul>
  * 
  * <p>Some elevator behavior is inherent and cannot be controlled:
- * <ul><i>Door opening & closing.</i> Elevator doors automatically open when arriving at a floor and 
+ * <ul><li><i>Door opening and closing.</i> Elevator doors automatically open when arriving at a floor and
  * close before departing. When passengers are waiting to board an elevator, the doors will 
- * stay open until all passengers are boarded. Each open and close action takes 1 second.</ul>
- * <ul><i>Elevator buttons.</i> Arriving passengers press the appropriate elevator button and the 
- * light is automatically turned off when the elevator arrives at the associated floor.</ul>
- * <ul><i>Elevator speed and acceleration.</i> The elevator will automatically accelerate to the maximum 
- * speed possible until there is only enough time left to slow down to arrive at the target floor.</ul>
+ * stay open until all passengers are boarded. Each open and close action takes 1 second.</li></ul>
+ * <ul><li><i>Elevator buttons.</i> Arriving passengers press the appropriate elevator button and the 
+ * light is automatically turned off when the elevator arrives at the associated floor.</li></ul>
+ * <ul><li><i>Elevator speed and acceleration.</i> The elevator will automatically accelerate to the maximum 
+ * speed possible until there is only enough time left to slow down to arrive at the target floor.</li></ul>
  * 
  * <p>There is substantial elevator status information available:
- * <ul><i>Elevator speed.</i> The current speed of the elevator. Positive speed indicates an elevator
- * heading up while negative indicates an elevator going down.</ul>
- * <ul><i>Elevator acceleration.</i> The acceleration of the elevators.</ul>
- * <ul><i>Elevator button status.</i> For each elevator, which floor buttons on the elevator have 
- * been pressed.</ul>
- * <ul><i>Elevator position.</i> The current position of the elevator, both in feet and to 
- * the closest floor. Position is measured in feet above ground floor with zero being the bottom.</ul>
- * <ul><i>Elevator door status.</i> Whether the doors for an elevator are open or closed. The 
- * status may also indicate a transition between open or closed.</ul>
- * <ul><i>Elevator target.</i> The current floor target of the elevator as set by the controller. The
- * elevator will travel to that target and stop until directed to the next target.</ul>
- * <ul><i>Elevator committed direction.</i> The current committed direction of the elevator. Elevators
+ * <ul><li><i>Elevator speed.</i> The current speed of the elevator. Positive speed indicates an elevator
+ * heading up while negative indicates an elevator going down.</li></ul>
+ * <ul><li><i>Elevator acceleration.</i> The acceleration of the elevators.</li></ul>
+ * <ul><li><i>Elevator button status.</i> For each elevator, which floor buttons on the elevator have 
+ * been pressed.</li></ul>
+ * <ul><li><i>Elevator position.</i> The current position of the elevator, both in feet and to 
+ * the closest floor. Position is measured in feet above ground floor with zero being the bottom.</li></ul>
+ * <ul><li><i>Elevator door status.</i> Whether the doors for an elevator are open or closed. The 
+ * status may also indicate a transition between open or closed.</li></ul>
+ * <ul><li><i>Elevator target.</i> The current floor target of the elevator as set by the controller. The
+ * elevator will travel to that target and stop until directed to the next target.</li></ul>
+ * <ul><li><i>Elevator committed direction.</i> The current committed direction of the elevator. Elevators
  * responding to a passenger floor button must have a committed direction, up or down. Otherwise, 
  * passengers wishing to go up would board the same elevators as passengers wishing to go down. 
  * Accordingly, each elevator can have an up, down or uncommitted committed direction set in response 
- * to passenger travel..</ul>
- * <ul><i>Elevator floor service.</i> Whether a particular elevator services a particular floor. When 
+ * to passenger travel..</li></ul>
+ * <ul><li><i>Elevator floor service.</i> Whether a particular elevator services a particular floor. When 
  * elevators are allowed to only service certain floors, this can help to achieve greater passenger 
- * service. Every elevator must service the ground floor.</ul>
- * <ul><i>Elevator weight.</i> This provides the current weight of the elevator less the weight of the 
+ * service. Every elevator must service the ground floor.</li></ul>
+ * <ul><li><i>Elevator weight.</i> This provides the current weight of the elevator less the weight of the 
  * empty elevator – hence the weight of the passengers on board. This can be useful for detecting
- * when the elevator is getting full.</ul>
+ * when the elevator is getting full.</li></ul>
  * 
  * <p>The elevators can be controlled by setting the following parameters:
- * <ul><i>Elevator target.</i> Sets the floor target of a particular elevator. Typically, this target 
+ * <ul><li><i>Elevator target.</i> Sets the floor target of a particular elevator. Typically, this target 
  * is set in response to a given passenger selecting a floor up or down button, or selecting a floor 
  * button in an elevator. The elevator will travel to that target and stop until directed to the 
- * next target.</ul>
- * <ul><i>Elevator committed direction.</i> Sets the current committed direction of the elevator. This 
+ * next target.</li></ul>
+ * <ul><li><i>Elevator committed direction.</i> Sets the current committed direction of the elevator. This 
  * is set so that passengers board elevators going in their desired direction. This is indicated by the
  * controller by whether the up or down floor button has been pressed on a given floor. This is
  * an important concept and it should be recognized that in some cases, an elevator may be 
  * going down, for example, but its committed direction is up – reflecting the fact it has been 
  * dispatched down to pick up a passenger who is traveling up. Whenever an elevator is being
- * dispatched to a target floor, it should have a committed direction set.</ul>
- * <ul><i>Elevator floor service.</i> Sets whether a particular elevator services a particular floor. 
+ * dispatched to a target floor, it should have a committed direction set.</li></ul>
+ * <ul><li><i>Elevator floor service.</i> Sets whether a particular elevator services a particular floor. 
  * In larger buildings, it is typically better to set elevator service to a particular subset of floors. 
- * Note that the ground floor will always get service.</ul>
+ * Note that the ground floor will always get service.</li></ul>
  *
  * @version 0.1
  */
